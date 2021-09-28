8e7c778cb43689d56bb8c548cc6bb6628456ed1a
sudhan6 <rbsudhan2001@gmail.com>
2021-09-28 18:56:14 +0530

    Js datatypes

---
+++ app.js
@@ -0,0 +1,15 @@
+// string
+console.log(typeof"hello world")
+console.log(typeof"d")
+
+//Numbers
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof 12365549933258)
+console.log(typeof -1)
+console.log(typeof -2.5)
+
+//boolean
+console.log(typeof true)
+console.log(typeof false)
+
