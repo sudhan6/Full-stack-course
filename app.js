3f6923b29f44a562abf21d59bb05cdede6228c9e
sudhan6 <rbsudhan2001@gmail.com>
2021-09-28 19:41:43 +0530

    operators and array

--- app.js
+++ app.js
@@ -1,15 +1,56 @@
-// string
-console.log(typeof"hello world")
-console.log(typeof"d")
+// array
+var arr = [1,2,3,4]
 
-//Numbers
-console.log(typeof 1)
-console.log(typeof 1.2)
-console.log(typeof 12365549933258)
-console.log(typeof -1)
-console.log(typeof -2.5)
+console.log(typeof arr)
+arr.push(5)
+console.log(arr)
+console.log(arr.length)
 
-//boolean
-console.log(typeof true)
-console.log(typeof false)
+//operator
+var a=2
+var b=10
 
+//arithematic operator
+console.log('a+b=',a+b)
+console.log('b-a=',b-a)
+console.log('b/a=',b/a)
+console.log('a*b=',a*b)
+console.log('b**a=',b**a)
+console.log('b%a=',b%a)
+
+// Assignment operator
+console.log('b ',b)
+b+=10
+console.log('b+=1',b)
+b++
+console.log('b++',b)
+b-=10
+console.log('b-=1',b)
+b--
+console.log('b--',b)
+
+// comparison operator
+var d = 10
+var e = 2
+var f ='10'
+
+console.log(d>e)
+console.log(d<e)
+console.log(d>=e)
+console.log(d<=e)
+console.log(d==e)
+console.warn(d == f)
+console.warn(d===f)
+
+// logical (and) operator
+
+console.log('true && true ',true && true)
+console.log('true && false ',true &&false)
+console.log('false && true ',false && true)
+console.log('false && false ',false && false)
+
+// logical (or) operator
+console.log('true || true ',true || true)
+console.log('true || false ',true || false)
+console.log('false || true ',false || true)
+console.log('false || false ',false || false)
