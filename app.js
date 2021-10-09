1ea6250208231bb83acb582726be0f5b05397645
sudhan6 <rbsudhan2001@gmail.com>
2021-10-09 19:27:05 +0530

    module in node.js

--- app.js
+++ app.js
@@ -1,55 +1,7 @@
-const a = document.querySelectorAll('input')[0]
-const b = document.querySelectorAll('input')[1]
-const add = document.querySelector('#add')
-const subtract = document.querySelector('#subtract')
-const multiply = document.querySelector('#multiply')
-const resultBox = document.querySelector('.result')
+const{add,subtract,multiply,divide,message} = require('./utils')
 
-const sum = () => {
-    const result = parseInt(a.value) + parseInt(b.value)
-    resultBox.innerHTML = result
-}
-
-const product = () => {
-    const result = parseInt(a.value) * parseInt(b.value)
-    resultBox.innerHTML = result
-}
-
-
-const calculate = (event, operation) => {
-  //  console.log(event.target)
-  //  if (operation == 'add') {
-  //      console.log('adding')
-  //  } else if (operation == 'multiply') {
-  //      console.log('multiply')
-  //  }
-
-  switch (operation){
-      case "add":
-          resultBox.innerHTML = parseInt(a.value) + parseInt(b.value)
-         
-          break
-      case "multiply":
-        resultBox.innerHTML = parseInt(a.value) * parseInt(b.value)
-         
-         break
-      case "subtract":
-            resultBox.innerHTML = parseInt(a.value) - parseInt(b.value)
-             
-             break
-      default:
-        resultBox.innerHTML = "Not a valid operation"
-  }
-}
-
-
-add.addEventListener('click', (event) => {
-    calculate(event, 'add')
-})
-
-subtract.addEventListener('click',(event)=>{
-    calculate(event, 'subtract') 
-})
-multiply.addEventListener('click', (event) => {
-    calculate(event, 'multiply')
-})
+add()
+subtract()
+multiply()
+divide()
+console.log(message)
